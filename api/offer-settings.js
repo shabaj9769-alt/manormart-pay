d65e2ff4a568d7a21994baeee0158c04c30f424a
shabaj9769-alt <shabaj9769@gmail.com>
2026-09-25 15:43:04 +0530

    Update offer-settings.js

--- api/offer-settings.js
+++ api/offer-settings.js
@@ -4,7 +4,7 @@ if (!admin.apps.length) {
   try {
     admin.initializeApp({
       credential: admin.credential.cert(JSON.parse(process.env.FIRE_JSON)),
-      databaseURL: process.env.FIRE__URL
+      databaseURL: process.env.FIREBASE_DATABASE_URL
     });
   } catch (err) {
     console.error('Firebase init error:', err);
